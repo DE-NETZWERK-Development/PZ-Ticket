4e55630fedb70894ca2938ddd14de8911ca60be7
LouiLetsPlayPro <51512114+LouiLetsPlayPro@users.noreply.github.com>
2022-10-29 23:34:30 +0200

    Update embed.js

--- src/embedhandler/embed.js
+++ src/embedhandler/embed.js
@@ -18,25 +18,61 @@ const emddata = require('./embed.json')
  * @param {object} data.reports Die Report Daten
  * 
  * @param {boolean} data.reports.tar If Reports founded
+ * @param {Array} data.reports.rids Die Report daten
  * 
- * @return {discord.EmbedBuilder} 
+ * @return {discord.Embed} 
  */
 
-module.exports.run = (type, data) => {
+module.exports.embedh = async (type, data) => {
 
     if (type == "reportids") {
-        if(!data.title) data.title = emddata.reports.title
-        if(!data.color) data.color = emddata.reports.color
-        if(!data.footer) data.title = emddata.reports.footer
+        if (!data.title) data.title = emddata.reports.title
+        if (!data.color) data.color = emddata.reports.color
+        if (!data.footer) data.footer = emddata.reports.footer
+        console.log(data)
         if (data.reports.tar == false) {
-            if(!data.fields) data.fields = emddata.reports.fields.nreports
-            /**
-             * TODO: Author -> if ! not in embed
-             * TODO: Image -> if ! not in embed
-             * TODO: Thumbnaul -> if ! not in embed
-             */
+            if (!data.fields) data.fields = emddata.reports.fields.nreports
+
+            const embed = new discord.EmbedBuilder()
+                .setTitle(data.title)
+                .setColor(data.color)
+                .setFooter(data.footer)
+                .setFields(data.fields)
+            if (!data.author) { } else { embed.setAuthor(data.author) }
+            if (!data.image) { } else { embed.setImage(data.image) }
+            if (!data.thumbnail) { } else { embed.setThumbnail(data.thumbnail) }
+            return embed
         }
-        if(!data.fields) data.fields = emddata.reports.fields
-        if(!data.description) data.description = emddata.reports.description
+
+        if (data.reports.rids.length == 0) {
+            return interaction.reply({ embeds: [await this.embedh("reportids", { reports: { tar: false } })] })
+        }
+
+        var rids = "";
+        for (let i = 0; i < data.reports.rids.length; i++) {
+            var t = rids;
+            rids = rids + data.reports.rids[i] + "\n"
+            if (rids >= 4000) {
+                rids = t
+            }
+        }
+
+        if (!data.description) data.description = emddata.reports.description
+        if (!data.fields) data.fields = emddata.reports.fields.reports
+
+        if (data.description.includes("${rids}")) data.description = data.description.replace("${rids}", rids)
+        if (data.fields.value.includes("${rlength}")) data.fields.value = data.fields.value.replace("${rlength}", data.reports.rids.length)
+
+        const embed = new discord.EmbedBuilder()
+            .setTitle(data.title)
+            .setColor(data.color)
+            .setFooter(data.footer)
+            .setFields(data.fields)
+            .setDescription(data.description)
+        if (!data.author) { } else { embed.setAuthor(data.author) }
+        if (!data.image) { } else { embed.setImage(data.image) }
+        if (!data.thumbnail) { } else { embed.setThumbnail(data.thumbnail) }
+
+        return embed;
     }
 }
